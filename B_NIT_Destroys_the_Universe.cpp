fb1260974881918ab09ee1770272ab07e1f055bd
CV.LIKHITH <143379120+CodeJeet2005LIKHITH@users.noreply.github.com>
2024-03-13 21:14:10 +0530

    Update B_NIT_Destroys_the_Universe.cpp

--- B_NIT_Destroys_the_Universe.cpp
+++ B_NIT_Destroys_the_Universe.cpp
@@ -9,6 +9,9 @@ void solve(){
     for(int i=1; i <= n; i++){
         if(arr[i-1] == 0 && arr[i] != 0)count++;
     }
+    // maximum operations will be 2 only
+    // think
+    count = min(count,2);
     cout<<count;
 }
 int main(){
